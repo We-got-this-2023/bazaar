4f94f628a07ee0a55623f27132de4c9c580cc286
Max <kanielrkirby@gmail.com>
2023-01-25 08:58:23 -0600

    Added fake controllers to prevent errors when running backend

---
+++ server/routes/productRoutes.js
@@ -0,0 +1,23 @@
+import express from 'express'
+
+const router = express.Router()
+
+// ----------------------------------------
+// Functions to prevent errors until the controllers are created
+let getProducts, getProduct, createProduct, deleteProduct, updateProduct
+getProducts =
+  getProduct =
+  createProduct =
+  deleteProduct =
+  updateProduct =
+    () => {}
+// ----------------------------------------
+
+// "/" refers to "/products"
+router.get('/', getProducts)
+router.get('/:id', getProduct)
+router.post('/', createProduct)
+router.delete('/:id', deleteProduct)
+router.patch('/:id', updateProduct)
+
+export default router
