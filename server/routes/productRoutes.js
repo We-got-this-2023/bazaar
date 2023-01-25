c1e3007562912ac6ac12ca12c46d4886c6f90d93
Max <kanielrkirby@gmail.com>
2023-01-25 10:14:16 -0600

    Wrote prettier to backend

--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -1,23 +1,23 @@
-import express from 'express'
+import express from "express";
 
-const router = express.Router()
+const router = express.Router();
 
 // ----------------------------------------
 // Functions to prevent errors until the controllers are created
-let getProducts, getProduct, createProduct, deleteProduct, updateProduct
+let getProducts, getProduct, createProduct, deleteProduct, updateProduct;
 getProducts =
   getProduct =
   createProduct =
   deleteProduct =
   updateProduct =
-    () => {}
+    () => {};
 // ----------------------------------------
 
 // "/" refers to "/products"
-router.get('/', getProducts)
-router.get('/:id', getProduct)
-router.post('/', createProduct)
-router.delete('/:id', deleteProduct)
-router.patch('/:id', updateProduct)
+router.get("/", getProducts);
+router.get("/:id", getProduct);
+router.post("/", createProduct);
+router.delete("/:id", deleteProduct);
+router.patch("/:id", updateProduct);
 
-export default router
+export default router;
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,20 +1,20 @@
-import express from 'express'
+import express from "express";
 
-const router = express.Router()
+const router = express.Router();
 
 // ----------------------------------------
 // Functions to prevent errors until the controllers are created
-let getUsers, getUser, createUser, deleteUser, updateUser
-getUsers = getUser = createUser = deleteUser = updateUser = () => {}
+let getUsers, getUser, createUser, deleteUser, updateUser;
+getUsers = getUser = createUser = deleteUser = updateUser = () => {};
 // ----------------------------------------
 
 // "/" refers to "/users"
-router.get('/', getUsers)
-router.get('/:id', getUser)
-router.post('/', createUser)
-router.delete('/:id', deleteUser)
-router.patch('/:id', updateUser)
+router.get("/", getUsers);
+router.get("/:id", getUser);
+router.post("/", createUser);
+router.delete("/:id", deleteUser);
+router.patch("/:id", updateUser);
 
-export default router
+export default router;
 
 // Functions to prevent errors until the controllers are created
