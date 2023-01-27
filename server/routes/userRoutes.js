07daf0c59952f224afe75ede7f005395946e0f93
Max <kanielrkirby@gmail.com>
2023-01-27 13:12:05 -0600

    Debugging controller

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -9,12 +9,6 @@ import {
 
 const router = express.Router();
 
-// ----------------------------------------
-// Functions to prevent errors until the controllers are created
-let getUsers, getUser, createUser, deleteUser, updateUser;
-getUsers = getUser = createUser = deleteUser = updateUser = () => {};
-// ----------------------------------------
-
 // "/" refers to "/users"
 router.get("/", getUsers);
 router.get("/:id", getUser);
