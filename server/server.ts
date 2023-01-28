675806e5a4cbb8c7f1b83062616eec89591f5214
Emelit0 <stollk811@gmail.com>
2023-01-28 13:45:31 +0100

    add dependencies and migrate to typescript

---
+++ server/server.ts
@@ -0,0 +1,28 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import { PrismaClient } from "@prisma/client";
+import userRoutes from "./routes/userRoutes.js";
+import productRoutes from "./routes/productRoutes.js";
+
+export const prisma = new PrismaClient();
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+const port = 3000;
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Hello, world!");
+});
+
+// TODO set up the user and products routes
+app.use("/users", userRoutes);
+app.use("/products", productRoutes);
+
+app.listen(port, () => {
+  console.log("Server running . . .");
+});
