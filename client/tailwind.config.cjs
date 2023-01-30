3a254a8a4c2cfcee78cd09cf3d24476572c8c010
Max <kanielrkirby@gmail.com>
2023-01-29 18:02:50 -0600

    Added dark mode

--- client/tailwind.config.cjs
+++ client/tailwind.config.cjs
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,ts,js,jsx,tsx}"],
+  darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          500: "#a92425",
+        },
+        black: {
+          0: "#000",
+          200: "#222",
+        },
+      },
+    },
   },
   plugins: [],
 };
