301641c86967c366f610a8ead24aa9cbba154b21
Emelit0 <stollk811@gmail.com>
2023-01-30 18:44:13 +0100

    add crud on product controllers

--- server/controllers/productController.ts
+++ server/controllers/productController.ts
@@ -1,24 +1,77 @@
 import { Request, Response } from "express";
 import prisma from "../prisma/prisma.js";
 
-async function getProducts(req: Request, res: Response) {
-  res.send("getProducts");
-}
+export const getProducts = async (req: Request, res: Response) => {
+  try {
+    const products = await prisma.product.findMany();
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(404).json({ message: "Products not found" });
+  }
+};
 
-async function getProduct(req: Request, res: Response) {
-  res.send("getProduct");
-}
+export const getProduct = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const product = await prisma.product.findUnique({
+      where: { id: Number(id) },
+    });
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(404).json({ message: "Product not found" });
+  }
+};
 
-async function createProduct(req: Request, res: Response) {
-  res.send("createProduct");
-}
+export const createProduct = async (req: Request, res: Response) => {
+  try {
+    const { name, price, userId } = req.body;
+    const product = await prisma.product.create({
+      data: {
+        name,
+        price: Number(price),
+        user: {
+          connect: {
+            id: Number(userId),
+          },
+        },
+      },
+    });
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(404).json({ message: "Product not found" });
+  }
+};
 
-async function deleteProduct(req: Request, res: Response) {
-  res.send("deleteProduct");
-}
+export const deleteProduct = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const product = await prisma.product.delete({
+      where: { id: Number(id) },
+    });
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(404).json({ message: "Product not found" });
+  }
+};
 
-async function updateProduct(req: Request, res: Response) {
-  res.send("updateProduct");
-}
-
-export { getProducts, getProduct, createProduct, deleteProduct, updateProduct };
+export const updateProduct = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const { name, price, userId } = req.body;
+    const product = await prisma.product.update({
+      where: { id: Number(id) },
+      data: {
+        name,
+        price,
+        user: {
+          connect: {
+            id: Number(userId),
+          },
+        },
+      },
+    });
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(404).json({ message: "Product not found" });
+  }
+};
