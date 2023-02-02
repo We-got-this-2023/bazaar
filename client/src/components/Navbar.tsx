50a67cc6f18161d5f57d18f98b18e13b10bc66c3
Niimaii <nimaicgomes@gmail.com>
2023-02-01 23:58:32 -0300

    Touch ups

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,7 +1,13 @@
+import Logo from "../assets/Logo";
+
 export default function Navbar() {
   return (
-    <nav>
-      <h1>Navbar</h1>
+    <nav className="nav_section">
+      <div className="nav_logoSection">
+        <Logo className="nav_logo" />
+        <p>Bazaar</p>
+      </div>
+      <div className="nav_searchBar">BIG SEARCH BAR</div>
     </nav>
   );
 }
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,3 +1,15 @@
-export default function Home() {
-  return <h1>Home</h1>;
-}
+import React from "react";
+import Logo from "../assets/Logo";
+
+const Home = () => {
+  return (
+    <main>
+      <section className="home_content">
+        <button>Home</button>
+        <div className="home_itemScroll"></div>
+      </section>
+    </main>
+  );
+};
+
+export default Home;
