7b4b1c926bd09a4fee7ac4cce34b7304e36a95e5
Emelit0 <stollk811@gmail.com>
2023-02-04 19:34:59 +0100

    add verificattion to some routes

--- server/routes/authRoutes.ts
+++ server/routes/authRoutes.ts
@@ -1,8 +1,8 @@
 import express from "express";
-import { login } from "../controllers/authController.js";
+import { signUp } from "../controllers/authController.js";
 
 const router = express.Router();
 
-router.post("/login", login);
+router.post("/login", signUp);
 
 export default router;
