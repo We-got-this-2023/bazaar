da6ced4bd5398407b7649ecb706dcb98f47a7e67
Max <kanielrkirby@gmail.com>
2023-02-09 17:56:15 -0600

    Fixed visual bug with labels
    The bug was caused by state being lost. Use Hook Form was overwriting the ref I had set, and I did not know about this. After looking at their documentation, I found a workaround

--- client/src/components/Form.tsx
+++ client/src/components/Form.tsx
@@ -1,4 +1,4 @@
-import React, { createElement } from "react";
+import React from "react";
 import { FormProvider, useForm } from "react-hook-form";
 
 interface FormProps {
@@ -10,21 +10,11 @@ interface FormProps {
 export function Form({ defaultValues, children, onSubmit }: FormProps) {
   if (!children) return <></>;
   const methods = useForm({ defaultValues });
-  const { handleSubmit, register } = methods;
+  const { handleSubmit } = methods;
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      <FormProvider {...methods}>
-        {React.Children.map(children, (child: JSX.Element) => {
-          if (child?.type?.name === "Input")
-            return createElement(child.type, {
-              register,
-              key: child.props.name,
-              ...child.props,
-            });
-          return child;
-        })}
-      </FormProvider>
+      <FormProvider {...methods}>{children}</FormProvider>
     </form>
   );
 }
