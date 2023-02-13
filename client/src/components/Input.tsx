e9a58e97a103fefea9645c0f956d96215073cb77
Max <kanielrkirby@gmail.com>
2023-02-12 19:01:48 -0600

    Made some small changes to the forms.

--- client/src/components/Input.tsx
+++ client/src/components/Input.tsx
@@ -1,6 +1,7 @@
 import { ErrorMessage } from "@hookform/error-message";
-import { InputHTMLAttributes, useEffect, useRef, useState } from "react";
+import { InputHTMLAttributes, useRef, useState } from "react";
 import { FieldErrors, RegisterOptions, useFormContext } from "react-hook-form";
+import Warning from "../assets/Warning";
 
 interface FancyInputProps extends InputHTMLAttributes<HTMLInputElement> {
   errors?: FieldErrors;
@@ -33,14 +34,18 @@ export default function Input({ name, options, ...rest }: FancyInputProps) {
           }}
           onFocus={() => handleFocus(true)}
           onBlur={() => handleFocus(false)}
-          className="rounded-md border border-gray-300 px-4 pt-4 pb-1"
+          className={`rounded-md border px-4 pt-4 pb-2 ring-blue-300 focus:outline-none focus:ring-2 ${
+            name && errors[name]
+              ? "border-red-500 ring-red-300"
+              : "border-gray-300"
+          }`}
           placeholder=""
           aria-placeholder={rest.placeholder || ""}
         />
         <label
-          className={`absolute select-none capitalize opacity-60 transition-all duration-300 ease-out ${
+          className={`pointer-events-none absolute select-none capitalize opacity-60 transition-all duration-300 ease-out ${
             labelSmall
-              ? "top-0 pl-2 text-xs opacity-100"
+              ? "top-0 pl-2 text-xs opacity-80"
               : "top-1/2 -translate-y-1/2 pl-3 text-base"
           }`}
           htmlFor={rest.placeholder || name}
@@ -49,8 +54,11 @@ export default function Input({ name, options, ...rest }: FancyInputProps) {
         </label>
       </div>
       {name && errors[name] && (
-        <div className="text-xs text-red-500">
-          <ErrorMessage errors={errors} name={name} />
+        <div className="flex items-center gap-2 p-1 text-red-500">
+          <Warning className="h-5 w-5" />
+          <span className="text-sm">
+            <ErrorMessage errors={errors} name={name} />
+          </span>
         </div>
       )}
     </div>
