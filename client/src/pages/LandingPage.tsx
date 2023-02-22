024a0efb8677dbf0591312901da6c7e4a7a44df6
Max <kanielrkirby@gmail.com>
2023-02-22 12:45:08 -0600

    Fixed the X-Overflow on LandingPage

--- client/src/pages/LandingPage.tsx
+++ client/src/pages/LandingPage.tsx
@@ -2,13 +2,11 @@ import CardSlider from "../components/CardSlider";
 
 export default function LandingPage() {
   return (
-    <main className="flex h-screen flex-col justify-end font-logo">
-      <button className="mx-auto mb-20 h-20 w-96 rounded-2xl bg-brick-red font-display text-4xl font-semibold text-white">
+    <main className="flex h-full flex-grow flex-col items-center">
+      <button className="my-20 rounded-2xl bg-brick-red py-6 px-10 font-display text-4xl font-semibold text-white">
         Get Started
       </button>
-      <div className="pb-20">
-        <CardSlider />
-      </div>
+      <CardSlider />
     </main>
   );
 }
