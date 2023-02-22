e8358a2ff645836f25b8504dcbdd4421df3fa0b5
Max <kanielrkirby@gmail.com>
2023-02-22 12:45:09 -0600

    Added links for the Carousel cards

--- client/src/components/CardSlider.tsx
+++ client/src/components/CardSlider.tsx
@@ -6,21 +6,25 @@ import "swiper/css";
 import "swiper/css/pagination";
 
 // import required modules
+import { Link } from "react-router-dom";
 import { A11y, Autoplay, Keyboard, Navigation, Pagination } from "swiper";
 
-export default function CardSlider() {
+export default function CardSlider({ className }: { className?: string }) {
   let images = [
     {
       image: "electronics.jpg",
       name: "Electronics",
+      link: "/search/query=electronics",
     },
     {
       image: "glassware.jpg",
       name: "Glassware",
+      link: "/search/query=glassware",
     },
     {
       image: "apparels.jpeg",
       name: "Clothing",
+      link: "/search/query=clothing",
     },
   ];
 
@@ -43,24 +47,26 @@ export default function CardSlider() {
       keyboard
       a11y={{ enabled: true }}
       modules={[A11y, Autoplay, Pagination, Keyboard, Navigation]}
-      className="p-10"
+      className={`w-full p-10 ${className || ""}`}
     >
-      {images.map((item, index) => {
+      {images.map(({ name, image, link }, index) => {
         return (
           <SwiperSlide
-            key={item.name + index}
+            key={name + index}
             className="aspect-[4/3] overflow-hidden rounded-[3em]"
           >
-            <img
-              className="aspect-[4/3] w-full rounded-[3em] object-cover shadow-xl"
-              src={item.image}
-              alt={`Image of ${item.name}`}
-            />
-            <div className="absolute bottom-0 flex w-full justify-center bg-black bg-opacity-30 p-4">
-              <h2 className="relative text-xl font-bold text-white [text-shadow:_0_0_10px_#000000b0]">
-                {item.name}
-              </h2>
-            </div>
+            <Link to={link}>
+              <img
+                className="aspect-[4/3] w-full rounded-[3em] object-cover shadow-xl"
+                src={image}
+                alt={`Image of ${name}`}
+              />
+              <div className="absolute bottom-0 flex w-full justify-center bg-black bg-opacity-30 p-4">
+                <h2 className="relative text-xl font-bold text-white [text-shadow:_0_0_10px_#000000b0]">
+                  {name}
+                </h2>
+              </div>
+            </Link>
           </SwiperSlide>
         );
       })}
