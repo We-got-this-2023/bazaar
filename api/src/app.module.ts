3dd455befd46aa66ef67fc69d38b7b72f1237e0d
Emelit0 <stollk811@gmail.com>
2023-02-26 22:47:54 +0100

    add dependencies to main

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from 'prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { ProductModule } from './product/product.module';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule, AuthModule, UsersModule, ProductModule],
   controllers: [AppController],
   providers: [AppService],
 })
