82c08e12e93e6f5e3bf90d52a8b6a64f6938a8c8
Max <kanielrkirby@gmail.com>
2023-03-12 17:16:39 -0500

    Fixed problem with the settings init

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,3 +1,4 @@
+initSettings();
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import React from "react";
 import ReactDOM from "react-dom/client";
@@ -7,7 +8,6 @@ import { AuthProvider } from "./context/AuthContext";
 import { MiscProvider } from "./context/MiscContext";
 import "./index.css";
 import { initSettings } from "./utils/settings";
-initSettings();
 
 const queryClient = new QueryClient();
 
