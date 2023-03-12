70ec362def5cc1352262f6856caa442e3588611a
Emelit0 <106512108+Emelit0@users.noreply.github.com>
2023-03-12 03:14:34 +0100

    adding product api (#51)
    * add product module
    
    * add product service and controller
    
    * added productDto and productParamsDto
    
    * use productModule
    
    * add more userServices
    
    * more user Endpoints
    
    * update users dto
    
    * add authorization to userRoutes
    
    *added more user functions & routes

---
+++ api/src/product/dto/productParams.dto.ts
@@ -0,0 +1,14 @@
+export class ProductParamsDto {
+  q?: string;
+  p?: string;
+  clo?: string;
+  chi?: string;
+  s?: string;
+  o?: string;
+  t?: string;
+  rlo?: string;
+  rhi?: string;
+  stags?: string;
+  atags?: string;
+  ntags?: string;
+}
