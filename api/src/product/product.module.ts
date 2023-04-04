394fa14d60f96afa513669ee957e9f21bbaa1dce
Emelit0 <stollk811@gmail.com>
2023-04-04 23:45:56 +0200

    add seperate routes in new controller

--- api/src/product/product.module.ts
+++ api/src/product/product.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { JwtAuthGuard } from 'src/auth/jwt.guard';
 import { ProductController } from './product.controller';
 import { ProductService } from './product.service';
+import { SingleProductController } from './singleProduct.controller';
 
 @Module({
   providers: [ProductService, JwtAuthGuard],
-  controllers: [ProductController],
+  controllers: [ProductController, SingleProductController],
 })
 export class ProductModule {}
---
+++ api/src/product/singleProduct.controller.ts
@@ -0,0 +1,64 @@
+import { Controller, Query } from '@nestjs/common';
+import { ProductService } from './product.service';
+import {
+  Post,
+  UseInterceptors,
+  UploadedFile,
+  Body,
+  ParseFilePipe,
+  MaxFileSizeValidator,
+  FileTypeValidator,
+  Param,
+  Patch,
+  Get,
+  Res,
+  Delete,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { addProductDto } from './dto/addProduct.dto';
+import { Response } from 'express';
+import { ProductDto } from './dto/product.dto';
+
+@Controller('single')
+export class SingleProductController {
+  constructor(private readonly productService: ProductService) {}
+
+  @Get('product/find/:id')
+  findOne(
+    @Query('id') id: string,
+    @Res({ passthrough: true }) response: Response,
+  ) {
+    return this.productService.findOneProduct(id);
+  }
+
+  @Post(':id')
+  @UseInterceptors(FileInterceptor('file', { dest: './uploads' }))
+  addProduct(
+    @UploadedFile(
+      new ParseFilePipe({
+        validators: [
+          new MaxFileSizeValidator({ maxSize: 1000 * 1000 }),
+          new FileTypeValidator({ fileType: '(png|jpeg|jpg)' }),
+        ],
+        fileIsRequired: false,
+      }),
+    )
+    file: Express.Multer.File,
+    @Body() addProductDto: addProductDto,
+  ) {
+    return this.productService.addProduct(file, addProductDto);
+  }
+
+  @Patch(':id')
+  updateProduct(
+    @Param('id') id: string,
+    @Body() productDto: ProductDto,
+  ): Promise<ProductDto> {
+    return this.productService.updateProduct(id, productDto);
+  }
+
+  @Delete(':id')
+  deleteProduct(@Param('id') id: string): Promise<ProductDto> {
+    return this.productService.deleteProduct(id);
+  }
+}
