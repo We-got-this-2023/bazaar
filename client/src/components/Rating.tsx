00da4f670bc821243b26f2b04f17e648f97634ce
Piratey7007 <piratey7007@runbox.com>
2023-05-13 11:07:09 -0500

    ratings correctly show

--- client/src/components/Rating.tsx
+++ client/src/components/Rating.tsx
@@ -1,13 +1,13 @@
 export default function Rating({ rating }: { rating?: number }) {
   if (rating) {
     return (
-      <div className="flex gap-2">
+      <div className="flex gap-1">
         {(() => {
           let state: "full" | "half" | "empty";
           const stars = [];
-          for (let i = 1; i < 6; i++) {
-            if (rating > i) state = "full";
-            else if (rating - i > -0.2 && rating - i < 0.2) state = "half";
+          for (let i = 0; i < 5; i++) {
+            if (rating >= i + 0.2 && rating <= i + 0.8) state = "half";
+            else if (rating >= i + 0.8) state = "full";
             else state = "empty";
             stars.push(<Star key={i} state={state} />);
           }
@@ -17,7 +17,7 @@ export default function Rating({ rating }: { rating?: number }) {
     );
   } else {
     return (
-      <div className="flex gap-2">
+      <div className="flex gap-1">
         {(() => {
           const stars = [];
           for (let i = 1; i < 6; i++)
@@ -32,18 +32,18 @@ export default function Rating({ rating }: { rating?: number }) {
 function Star({ state }: { state: "empty" | "full" | "half" }) {
   return (
     <svg
-      className="w-14"
+      className="w-8"
       viewBox="0 0 96 91"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
       <path
         d="M77.3893 90.4509L48 69.0983V0L59.2257 34.5491H95.5529L66.1636 55.9017L77.3893 90.4509Z"
-        fill={state === "empty" ? "#C0C0C0" : "#F7EF42"}
+        fill={state === "full" ? "#F7EF42" : "#C0C0C0"}
       />
       <path
         d="M18.6107 90.4509L48 69.0983V0L36.7743 34.5491H0.447144L29.8364 55.9017L18.6107 90.4509Z"
-        fill={state === "full" ? "#F7EF42" : "#C0C0C0"}
+        fill={state === "empty" ? "#C0C0C0" : "#F7EF42"}
       />
     </svg>
   );
